7fe66320e1959421494dbedc82c5a7574de99b92
mphipps2017 <mnphipps98@gmail.com>
2019-11-02 13:43:46 -0500

    Started working on the Home scene

--- src/frontend/src/components/Event/index.js
+++ src/frontend/src/components/Event/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import stlyes from './style.css';
 
 const Event = (props) => {
-    console.log(props);
     return(
         <div className="event-container">
-            <div className="event-info">
-                <div className="event-title">
-                    <a>The good event</a>
+            <div className="event-title">
+                <p>{props.title}</p>
+            </div>
+            <div className="event-footer">
+                <div className="event-location">
+                    <p>{props.address}</p>
                 </div>
-                <div className="event-footer">
-                    <div className="event-location">
-                        <a>2700 Lincoln Way</a>
-                    </div>
-                    <div className="event-attendees">
-                        <a>Attendees: 365</a>
-                    </div>
+                <div className="event-attendees">
+                    <p>Attendees: {props.attendees}</p>
                 </div>
             </div>
         </div>
--- src/frontend/src/scenes/Home/index.js
+++ src/frontend/src/scenes/Home/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
-import './App.css';
+import stlyes from './style.css';
+import Event from '../../components/Event/index.js';
 
+const Home = () => {
+    return(
+        <div className="home-page">
+            <Event title={'The Good Event'} address={'2700 Lincoln Way'} attendees={365}/>
+        </div>
+    );
+}
+
+export default Home
