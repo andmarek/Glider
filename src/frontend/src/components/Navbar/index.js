b8330dbfa64669d938f2f67944027c36084d858b
Eric Marcanio <ericmarcanio@gmail.com>
2019-11-02 13:41:16 -0500

    Navbar

--- src/frontend/src/components/Navbar/index.js
+++ src/frontend/src/components/Navbar/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import styles from './style.css' ;
 
-const NavComponent = React.createClass({
-    //Using href links to new pages
-    render: function () {
-        return (
-            <nav>
-                <div className="mainDiv">
-                    <a href='#'>Login</a>
-                    <a href='#'>Register</a>
-                </div>
-            </nav>
-        );
+const NavComponent = (props) => {
+    return(
+        <nav>
+            <div className="NavBarLinks">
+                <link to='/'>
+                    <div className={"link"+ (props.location.pathname === "/" ? " active": "")}>Home</div>
+                </link>
+                <link to='/signIn'>
+                    <div className={"link"+ (props.location.pathname === "/" ? " active": "")}>Sign In</div>
+                </link>
+                <link to='/Register'>
+                    <div className={"link"+ (props.location.pathname === "/" ? " active": "")}>Register</div>
+                </link>
+            </div>
+        </nav>
+    );
 
-    },
+}
 
 
-
-
-});
+export default NavComponent;
